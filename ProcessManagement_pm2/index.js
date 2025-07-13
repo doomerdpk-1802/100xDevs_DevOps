655fde8ca0571b42a8ce45bdb3b192c1f48ec539
root <root@DEEPAK>
2025-07-13 22:08:28 +0530

    Process Management pm2

---
+++ ProcessManagement_pm2/index.js
@@ -0,0 +1,13 @@
+// npm i -g pm2 - to install pm2
+// pm2 start index.js
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
