ae2d937b15df0dd9c135ee4cc86c9d9a7f3bca19
root <root@DEEPAK>
2025-07-13 23:16:35 +0530

    ngrok

---
+++ ngrok_test/index.js
@@ -0,0 +1,12 @@
+// ngrok http 3000
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
